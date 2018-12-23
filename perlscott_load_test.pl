033c5a83ed08f4071f9e68cb008c710ea592ef30
pdxiv <pdxiv@github.com>
2018-12-23 19:27:37 +0100

    added loading of remaining data

--- perlscott_load_test.pl
+++ perlscott_load_test.pl
@@ -8,42 +8,7 @@ our $VERSION = '1.0.0';
 
 Readonly::Scalar my $ACTION_ENTRIES => 8;
 
-# my $game_file = '1_baton.dat';
-# my $game_file = '2_timemachine.dat';
-# my $game_file = '3_arrow1.dat';
-# my $game_file = '4_arrow2.dat';
-# my $game_file = '5_pulsar7.dat';
-# my $game_file = '6_circus.dat';
-# my $game_file = '7_feasibility.dat';
-# my $game_file = '8_akyrz.dat';
-# my $game_file = '9_perseus.dat';
-# my $game_file = 'A_tenlittleindians.dat';
-# my $game_file = 'adv01.dat';
-# my $game_file = 'adv02.dat';
-# my $game_file = 'adv03.dat';
-# my $game_file = 'adv04.dat';
-# my $game_file = 'adv05.dat';
-# my $game_file = 'adv06.dat';
-# my $game_file = 'adv07.dat';
-# my $game_file = 'adv08.dat';
-# my $game_file = 'adv09.dat';
-# my $game_file = 'adv10.dat';
-# my $game_file = 'adv11.dat';
-# my $game_file = 'adv12.dat';
-# my $game_file = 'adv13.dat';
-# my $game_file = 'adv14a.dat';
-# my $game_file = 'B_waxworks.dat';
-# my $game_file = 'BOND.DAT';
-# my $game_file = 'BURGLAR.DAT';
-# my $game_file = 'CONQUEST.DAT';
-# my $game_file = 'desert.dat';
-# my $game_file = 'GAMMA.DAT';
-# my $game_file = 'MAROONED.DAT';
-# my $game_file = 'MINER.DAT';
-# my $game_file = 'quest1.dat';
-# my $game_file = 'romulan.dat';
-# my $game_file = 'sampler1.dat';
-# my $game_file = 'secret.dat';
+my $game_file = shift @ARGV;
 
 open my $handle, '<', $game_file or croak;
 my $file_content = do { local $INPUT_RECORD_SEPARATOR; <$handle> };
@@ -58,8 +23,6 @@ my $newline_pattern = qr/$unix|$apple|$dos/msx;
 # Replace newline in file with whatever the current system uses
 $file_content =~ s/$newline_pattern/$INPUT_RECORD_SEPARATOR/msxg;
 
-print $file_content;
-
 # extract fields from room entry from data file
 my $room_pattern = qr{
 \s+(-?\d+)
@@ -70,32 +33,32 @@ my $room_pattern = qr{
 \s+(-?\d+)
 \s*"([^"]*)"
 (.*)
-}msxs;
+}msx;
 
 # extract fields from object entry
 my $object_pattern = qr{
-\s*"([^"]*?)(?:/([^/]+)/)?"
+\s*\"([^"]*)"
 \s*(-?\d+)
 (.*)
-}msxs;
+}msx;
 
 # extract data from a verb or a noun
 my $word_pattern = qr{
-\s*"([*]?\S*?)"
+\s*"([*]?[^"]*?)"
 (.*)
-}msxs;
+}msx;
 
-# extract data from an action comment
-my $comment_pattern = qr{
+# extract data from a general text field
+my $text_pattern = qr{
 \s*"([^"]*)"
 (.*)
-}msxs;
+}msx;
 
 # extract a numerical value
 my $number_pattern = qr{
 \s*(-?\d+)
 (.*)
-}msxs;
+}msx;
 
 my $next = $file_content;
 my $game_bytes;
@@ -114,18 +77,26 @@ my @action_data;
 my @list_of_verbs_and_nouns;
 my @room_exit;
 my @room_description;
-( $game_bytes,          $next ) = $next =~ /$number_pattern/msxs;
-( $number_of_objects,   $next ) = $next =~ /$number_pattern/msxs;
-( $number_of_actions,   $next ) = $next =~ /$number_pattern/msxs;
-( $number_of_words,     $next ) = $next =~ /$number_pattern/msxs;
-( $number_of_rooms,     $next ) = $next =~ /$number_pattern/msxs;
-( $max_objects_carried, $next ) = $next =~ /$number_pattern/msxs;
-( $starting_room,       $next ) = $next =~ /$number_pattern/msxs;
-( $number_of_treasures, $next ) = $next =~ /$number_pattern/msxs;
-( $word_length,         $next ) = $next =~ /$number_pattern/msxs;
-( $time_limit,          $next ) = $next =~ /$number_pattern/msxs;
-( $number_of_messages,  $next ) = $next =~ /$number_pattern/msxs;
-( $treasure_room_id,    $next ) = $next =~ /$number_pattern/msxs;
+my @message;
+my @object_description;
+my @object_location;
+my @object_original_location;
+my @action_description;
+my $adventure_version;
+my $adventure_number;
+
+( $game_bytes,          $next ) = $next =~ /$number_pattern/msx;
+( $number_of_objects,   $next ) = $next =~ /$number_pattern/msx;
+( $number_of_actions,   $next ) = $next =~ /$number_pattern/msx;
+( $number_of_words,     $next ) = $next =~ /$number_pattern/msx;
+( $number_of_rooms,     $next ) = $next =~ /$number_pattern/msx;
+( $max_objects_carried, $next ) = $next =~ /$number_pattern/msx;
+( $starting_room,       $next ) = $next =~ /$number_pattern/msx;
+( $number_of_treasures, $next ) = $next =~ /$number_pattern/msx;
+( $word_length,         $next ) = $next =~ /$number_pattern/msx;
+( $time_limit,          $next ) = $next =~ /$number_pattern/msx;
+( $number_of_messages,  $next ) = $next =~ /$number_pattern/msx;
+( $treasure_room_id,    $next ) = $next =~ /$number_pattern/msx;
 
 # Actions
 {
@@ -137,7 +108,7 @@ my @room_description;
             # $action_data[$action_id][$action_id_entry] =
             #   read_number($handle);
             ( $action_data[$action_id][$action_id_entry], $next ) =
-              $next =~ /$number_pattern/msxs;
+              $next =~ /$number_pattern/msx;
             $action_id_entry++;
         }
         $action_id++;
@@ -149,7 +120,7 @@ my @room_description;
     my $word = 0;
     while ( $word < ( ( $number_of_words + 1 ) * 2 ) ) {
         my $input;
-        ( $input, $next ) = $next =~ /$word_pattern/msxs;
+        ( $input, $next ) = $next =~ /$word_pattern/msx;
         $list_of_verbs_and_nouns[ int( $word / 2 ) ][ $word % 2 ] = $input;
         $word++;
     }
@@ -159,9 +130,43 @@ my @room_description;
 {
     my $room = 0;
     while ( $room <= $number_of_rooms ) {
-        ( $room_exit[$room][0], $room_exit[$room][1], $room_exit[$room][2], $room_exit[$room][3], $room_exit[$room][4], $room_exit[$room][5], $room_description[$room], $next ) = $next =~ /$room_pattern/msxs;
-        print "%%% $room/$number_of_rooms $room_description[$room]\n";
+        ( $room_exit[$room][0], $room_exit[$room][1], $room_exit[$room][2], $room_exit[$room][3], $room_exit[$room][4], $room_exit[$room][5], $room_description[$room], $next ) = $next =~ /$room_pattern/msx;
         $room++;
     }
 }
 
+# Messages
+{
+    my $current_message = 0;
+    while ( $current_message <= $number_of_messages ) {
+        ( $message[$current_message], $next ) = $next =~ /$text_pattern/msx;
+        $current_message++;
+    }
+}
+
+# Objects
+{
+    my $object = 0;
+    while ( $object <= $number_of_objects ) {
+        ( $object_description[$object], $object_location[$object], $next ) = $next =~ /$object_pattern/msx;
+        $object_original_location[$object] = $object_location[$object];
+        $object++;
+    }
+}
+
+# Action descriptions
+{
+    my $action_counter = 0;
+    while ( $action_counter <= $number_of_actions ) {
+        ( $action_description[$action_counter], $next ) = $next =~ /$text_pattern/msx;
+        $action_counter++;
+    }
+}
+
+( $adventure_version, $next ) = $next =~ /$number_pattern/msx;    # Interpreter version
+( $adventure_number,  $next ) = $next =~ /$number_pattern/msx;    # Adventure number
+
+# Replace Ascii 96 with Ascii 34 in output text strings
+foreach ( ( @object_description, @message, @room_description ) ) {
+    s/`/"/msxg;
+}
